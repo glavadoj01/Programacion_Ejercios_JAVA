310e48a519fc22cc6252216a5be95dc834793bf2
glavadoj01 <gonzalolj@hotmail.es>
2025-06-12 09:00:17 +0200

    QuickFix

--- 3EV_Examen1/src/acceso_datos/ConectarBD.java
+++ 3EV_Examen1/src/acceso_datos/ConectarBD.java
@@ -8,7 +8,7 @@ public class ConectarBD {
 
     private static final String URL = "jdbc:mysql://localhost:3306/classicmodels_esp";
     private static final String USER = "root";
-    private static final String PASSWORD = "kcN4bLUM";
+    private static final String PASSWORD = "";
 
     public static Connection getConexion() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
